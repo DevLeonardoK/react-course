c238a6856aba8da6a610e8c9feeef7683c3671a0
Leonardo Kremer <168785661+DevLeonardoK@users.noreply.github.com>
2024-06-08 16:43:07 -0300

    js

---
+++ html/javascript/script.js
@@ -0,0 +1,51 @@
+//OPERADORES BÁSICOS
+// const valor = 10;
+// console.log("soma da const valor: ", valor + valor);
+// console.log("divisao da const valor: ", valor / valor);
+// console.log("multiplicação da const valor: ", valor * valor);
+// console.log("subtração da const valor: ", valor - valor);
+
+//OPERADORES LÓGICOS
+// let nota = 5;
+
+// switch (nota) {
+//   case nota < 5:
+//     console.log("Abaixo da média");
+//     break;
+//   case (nota = 5):
+//     console.log("Na média");
+//     break;
+//   default:
+//     console.log("Acima da média");
+// }
+
+//OPERADOR ? e :
+// const nota = 9;
+// nota > 5 ? console.log("apovado") : console.log("Reprovado");
+
+//VARIAVEIS
+
+// function calcular(x) {
+//   console.log(x);
+// }
+
+// calcular(57);
+
+//LAÇOS DE REPETIAÇÃO
+for (let i = 1; i <= 5; i++) {
+  console.log(`${i} x 5 == ${i * 5}`);
+}
+
+console.log("=================");
+const valores = [1, 2, 3, 4, 5];
+valores.forEach((value, index) => {
+  console.log(`${value} X 5 == ${value * 5}`);
+});
+
+console.log("=================");
+const valores2 = [1, 2, 3, 4, 5];
+const resultado = valores2.map((value) => {
+  return value * 5;
+});
+
+console.log(resultado);
