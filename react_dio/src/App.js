b6ed0f719fd355ad586aef9f3753b9fadfd70935
Leonardo Kremer <168785661+DevLeonardoK@users.noreply.github.com>
2024-06-16 20:49:39 -0300

    counter

--- react_dio/src/App.js
+++ react_dio/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  //Definindo os values das variaveis como 0, inicializa como 0
+  const increment = () => {
+    setCount(count + 1); //Definindo o state do counter //Ar.Func
+  };
+  const decrement = () => {
+    setCount(count - 1); //Definindo o state do counter //Ar.Func
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>This is our counter Application</div>
+      <div className="flex justify-center ">
+        <button onClick={decrement}>-</button>
+        <span>Clicks {count}</span>
+        <button onClick={increment}>+</button>
+      </div>
+    </>
   );
 }
 
